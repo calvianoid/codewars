712778bee6b6794263de48853c288485f3a8305f
Calviano Nathanael <calvianoid@gmail.com>
2022-01-07 21:41:35 +0700

    third day

---
+++ even-or-odd.js
@@ -0,0 +1,9 @@
+function even_or_odd(number) {
+    // if(number % 2 == 0){
+    //   return 'Even';
+    // } return 'Odd';
+
+    return number % 2 ? "Odd" : "Even";
+}
+
+console.log(even_or_odd(-8));
