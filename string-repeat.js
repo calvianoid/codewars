c1e15dda7b43d6e758ba7172440108412157d76d
Calviano Nathanael <calvianoid@gmail.com>
2022-01-09 17:51:26 +0700

    5th day

---
+++ string-repeat.js
@@ -0,0 +1,11 @@
+function repeatStr(n, s) {
+    // let result = "";
+    // for (let i = 0; i < n; i++) {
+    //     result += s;
+    // }
+    // return result;
+
+    return s.repeat(n);
+}
+
+console.log(repeatStr(5, "Hello"));
