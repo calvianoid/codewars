77e8a99247e6d47ec5bf970ef1cbb8d8e9c72424
Calviano Nathanael <calvianoid@gmail.com>
2022-01-11 13:30:10 +0700

    7th day

---
+++ find-the-odd-int.js
@@ -0,0 +1,18 @@
+function findOddInt(arr) {
+    let count = 0;
+    let newArr = arr.sort((a, b) => a - b);
+    for (let i = 0; i < newArr.length; i++) {
+        for (let j = 0; j < newArr.length; j++) {
+            if (newArr[i] == newArr[j]) {
+                count++;
+            }
+        }
+        if (count % 2 !== 0) {
+            return newArr[i];
+        }
+    }
+}
+
+// console.log(findOddInt([7])); //1
+// console.log(findOddInt([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])); //5
+// console.log(findOddInt([1, 1, 2])); //2
